94041b5a15c353972c9088584f9d5c7668da6b37
Andrew Park <acpark123@gmail.com>
2020-06-09 04:51:05 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,21 @@ def find_even_values(src)
   #
   # Output all even values in each nested array
 
+  even_values = []
+  outer_array_counter = 0
+  while outer_array_counter < src.length do
 
+    inner_array_counter = 0
+
+     while inner_array_counter < src[outer_array_counter].length do
+        if src[outer_array_counter][inner_array_counter].even?
+					even_values.push(src[outer_array_counter][inner_array_counter])
+				end
+     inner_array_counter += 1
+    end
+	outer_array_counter += 1
+  end
+	even_values
 
 
 
